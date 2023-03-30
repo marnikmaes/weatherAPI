e25c31763ec0e429c22a4226cfa3f30483380a1b
Marnik Maes <91118430+marnikmaes@users.noreply.github.com>
2023-03-30 11:10:08 +0200

    assets
    assets

--- weather-app/src/App.js
+++ weather-app/src/App.js
@@ -1,9 +1,14 @@
 import React from "react";
 
+const api = {
+  key: "e8113088e3bc77898249bcb4125303ff",
+  base: "https://api.openweathermap.org/data/2.5/"
+}
+
 function App() {
   return (
     <div className="App">
-      <h1>Hello world</h1>
+      <main></main>
     </div>
   );
 }
