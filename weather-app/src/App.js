383d1270ccc37ffb65b5a7f4a21e710e8693ad8c
Marnik Maes <91118430+marnikmaes@users.noreply.github.com>
2023-03-30 11:40:22 +0200

    SearchBarAndDate
    SearchBarAndDate

--- weather-app/src/App.js
+++ weather-app/src/App.js
@@ -1,9 +1,38 @@
 import React from "react";
 
 function App() {
+
+  const dateBuilder = (d) => {
+    let months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+    let days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+
+    let day = days[d.getDay()];
+    let date = d.getDate();
+    let month = months[d.getMonth()];
+    let year = d.getFullYear();
+
+    return `${day} ${date} ${month} ${year}`
+  }
   return (
+<<<<<<< Updated upstream
     <div className="App">
       <h1>Hello world</h1>
+=======
+    <div className="app">
+      <main>
+        <div className="search-box">
+          <input 
+            type="text" 
+            className="search-bar"
+            placeholder="Search..."
+          />
+        </div>
+        <div className="location-box">
+          <div className="location">New York City, US</div>
+          <div className="data">{dateBuilder(new Date())}</div>
+        </div>
+      </main>
+>>>>>>> Stashed changes
     </div>
   );
 }
