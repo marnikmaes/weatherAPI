e31c59504d7cafb5a2cdc83af7ae4b8e21aa8cab
Marnik Maes <91118430+marnikmaes@users.noreply.github.com>
2023-03-30 12:11:49 +0200

    Styling
    Styling

--- weather-app/src/App.js
+++ weather-app/src/App.js
@@ -30,7 +30,11 @@ function App() {
         </div>
         <div className="location-box">
           <div className="location">New York City, US</div>
-          <div className="data">{dateBuilder(new Date())}</div>
+          <div className="date">{dateBuilder(new Date())}</div>
+        </div>
+        <div className="weather-box">
+          <div className="temp">15°c</div>
+          <div className="weather">Sunny</div>
         </div>
       </main>
     </div>
